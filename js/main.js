8996b43d0fa1b174bfb3ed16291869ed8e3dd917
Yumari DH <yumari.diaz.herrera@gmail.com>
2025-08-22 22:42:32 -0600

    Se agrega función de agregar ppreguntas, funcionalidad en botones y contador de preguntas (Parte 2)

--- js/main.js
+++ js/main.js
@@ -25,30 +25,64 @@ const preguntas = [
 
 const preguntaN = document.getElementById("preguntaN");
 const espacioRadios = document.getElementById("espacioRadios");
+const instrucciones = document.getElementById("instrucciones");
+const btnAnterior = document.getElementById("btnAnterior");
+const btnSiguiente = document.getElementById("btnSiguiente");
 
 let preguntaActual = 0; //empezamos con la pregunta 1 (indice 0 del array)
 
-//función para agregar cada pregunta al html
-function mostrarPregunta() {
+//función para agregar cada pregunta y sus opciones al html
+function mostrarPregunta(preguntaActual) {
 
     //1. para insertar la pregunta:
     preguntaN.textContent = preguntas[preguntaActual].pregunta;
 
     //2. para insertar las opciones:
-
     //primero limpiamos lo anterior:
-    espacioRadios.innerHTML = ""; 
+    espacioRadios.innerHTML = "";
 
     //después recorrer cada una de las opciones de la pregunta actual para insertarlas:
     let opciones = preguntas[preguntaActual].opciones //arreglo de opciones
-    array.forEach(element => {
-        
-    });
-
-    espacioRadios.insertAdjacentHTML("beforeend", `<div class="form-check d-flex justify-content-center align-items-center mb-2">
+    opciones.forEach(element => {
+        espacioRadios.insertAdjacentHTML("beforeend", `<div class="form-check d-flex justify-content-center align-items-center mb-2">
                             <input class="form-check-input me-2" type="radio" name="radioDefault" id="radio1">
                             <label class="form-check-label" for="radioDefault1">
-                                ${preguntas[preguntaActual].opciones[0]}
+                                ${element}
                             </label>
                         </div>`)
+    });
 }//función mostrarPregunta
+
+mostrarPregunta(preguntaActual); //de inicio muestra la pregunta 1
+instrucciones.innerText = `Este es un cuestionario de ${preguntas.length} preguntas, puedes navegar entre las preguntas
+                            sin perder la respuesta seleccionada. Pregunta 1 de ${preguntas.length}`;
+
+btnSiguiente.addEventListener("click", function (event) {
+    event.preventDefault();
+    preguntaActual++
+
+    if (preguntaActual >= preguntas.length-1) {
+        preguntaActual = preguntas.length-1;
+    }
+        mostrarPregunta(preguntaActual);
+    
+    instrucciones.innerText = `Este es un cuestionario de ${preguntas.length} preguntas, puedes navegar entre las preguntas
+                            sin perder la respuesta seleccionada. Pregunta ${preguntaActual+1} de ${preguntas.length}`;
+
+});//btnSiguiente
+
+btnAnterior.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (preguntaActual <= 0) {
+        preguntaActual = 0;
+    } else {
+        preguntaActual--
+        mostrarPregunta(preguntaActual);
+    }//if-else
+
+    instrucciones.innerText = `Este es un cuestionario de ${preguntas.length} preguntas, puedes navegar entre las preguntas
+                            sin perder la respuesta seleccionada. Pregunta ${preguntaActual+1} de ${preguntas.length}`;
+});//btnAnterior
+
+console.log("Elemento instrucciones:", instrucciones);
